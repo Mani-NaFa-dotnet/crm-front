64a46127b15d6ac0d48a51dbdc7e0fcd3c7d9d03
Manichandra112 <sadavasthulamanichandra@gmail.com>
2026-02-12 12:09:08 +0530

    downloaded packages

--- crm-entry/src/api/axios.js
+++ crm-entry/src/api/axios.js
@@ -43,7 +43,7 @@ const api = axios.create({
   baseURL: "http://89.116.20.215:9090/",
   // baseURL: "https://crm.metagensoft.com/api/",
   // baseURL: "https://albertine-nonempathic-heaven.ngrok-free.dev",
-  // baseURL: "https://localhost:7246",
+  // baseURL: "https://localhost:8080",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
