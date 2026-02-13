335efade8e23b0708e7c1f2d438193edeeee5e8e
Manichandra112 <sadavasthulamanichandra@gmail.com>
2026-02-13 15:41:18 +0530

    removed comments

--- crm-entry/src/api/admin/users.api.js
+++ crm-entry/src/api/admin/users.api.js
@@ -1,33 +1,8 @@
-
-// import api from "../axios";
-
-// export const getAdminUsers = (params) => {
-//   return api.get("/api/admin/users", { params });
-// };
-
-// export const getAdminUserById = (userId) => {
-//   return api.get(`/api/admin/users/${userId}`);
-// };
-
-// export const getUserSecurity = (userId) => {
-//   return api.get(`/api/admin/users/${userId}/security`);
-// };
-
-// export const getUserAuditLogs = (userId, params) => {
-//   return api.get(`/api/admin/users/${userId}/audit-logs`, {
-//     params,
-//   });
-// };
-
-
-
 import api from "../axios";
-
-/* ========= USERS ========= */
 
 export const getAdminUsers = async (params) => {
   const res = await api.get("/api/admin/users", { params });
-  return res.data;
+  return res.data || [];
 };
 
 export const getAdminUserById = async (userId) => {
--- crm-entry/src/api/axios.js
+++ crm-entry/src/api/axios.js
@@ -1,49 +1,11 @@
-// // src/api/axios.js
-// import axios from "axios";
 
-// const api = axios.create({
-//   baseURL:   
-//   "https://albertine-nonempathic-heaven.ngrok-free.dev",
-// //  "https://localhost:7246",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// // Attach token on every request
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("accessToken");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// // Global 401 handling
-// api.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.clear();
-//       window.location.href = "/login";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
-
-// src/api/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://89.116.20.215:9090/",
+  // baseURL: "http://89.116.20.215:9090/",
   // baseURL: "https://crm.metagensoft.com/api/",
   // baseURL: "https://albertine-nonempathic-heaven.ngrok-free.dev",
-  // baseURL: "https://localhost:7246",
+  baseURL: "https://localhost:8080",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
